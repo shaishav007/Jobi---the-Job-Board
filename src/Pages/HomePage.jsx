e3911b424353756fdcf983963ff9a25fa7b697ad
shaishav007 <shaishav007@gmail.com>
2023-03-24 11:40:28 -0400

    How it works Section markup added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -46,6 +46,50 @@ const HomePage = () => {
           </ul>
         </div>
       </section>
+      <section className="getOver50">
+        <div className="wrapper">
+        <div className="flexParent">
+        <div className="placeHolderImage">
+              placeholder image here
+        </div>
+        <div className="getOver50Content">
+          <h3>Get Over 50,000+<br/> talented experts <br/>on jobi.</h3>
+          <h4>A full hybrid workforce management tools are yours to<br/>
+              use, as well as access our top 1% of talent.
+          </h4>
+          <ul className="getOver50Checklist">
+            <li>Seamless searching</li>
+            <li>Get top 3% experts for your project</li>
+            <li>Protected payments system</li>
+          </ul>
+          <a href="#">Post a Job</a>
+        </div>
+        </div>
+        </div>
+      </section>
+      <section className="howItWorks">
+        <div className="wrapper">
+          <h3>How it Works?</h3>
+          <ul className='howItWorksList'>
+            <li>
+              <div className="placeHolderIcon">placeholderIcon</div>
+              <h4>Create Account</h4>
+              <h5>Its very easy to open an account and start your journey.</h5>
+              
+            </li>
+            <li>
+              <div className="placeHolderIcon">placeholderIcon</div>
+                <h4>Complete your profile</h4>
+                <h5>Complete your profile with all the info to get client's attention.</h5>
+            </li>
+            <li>
+              <div className="placeHolderIcon">placeholderIcon</div>
+                <h4>Apply job or hire</h4>
+                <h5>Apply & get your preferable jobs with all the requirements and get it. </h5>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   )
 }
